fafd652df54561fc812b0ddc565e37e68cd1ab18
Victor Frye <victorfrye@outlook.com>
2023-09-25 12:41:45 -0400

    september 2023 update

--- scripts/Install-DevelopmentTools.ps1
+++ scripts/Install-DevelopmentTools.ps1
@@ -1,6 +1,8 @@
 function Install-PowerUserTools() {
     winget install --id=Microsoft.WindowsTerminal --source=winget
 
+    winget install --id=Microsoft.DevHome --source=winget
+
     winget install --id=JanDeDobbeleer.OhMyPosh --source=winget
 
     winget install --id=Microsoft.PowerToys --source=winget
@@ -8,7 +10,7 @@ function Install-PowerUserTools() {
     winget install --id=Neovim.Neovim --source=winget
 }
 
-function Install-CoreDotNetTools() {
+function Install-DotNetTools() {
     winget install --id=Microsoft.DotNet.SDK.7
 
     winget install --id=Microsoft.VisualStudioCode --source=winget
@@ -17,14 +19,11 @@ function Install-CoreDotNetTools() {
 
     winget install --id=Microsoft.NuGet --source=winget
 
-    winget install --id=Docker.DockerDesktop --source=winget
 }
 
-function Install-CoreJavaTools() {
-    winget install --id=EclipseAdoptium.Temurin.17.JDK --source=winget
-    [Environment]::SetEnvironmentVariable('JDK_17', $env:JAVA_HOME, 'Machine')
-
+function Install-JavaTools() {
     winget install --id=Microsoft.OpenJDK.17 --source=winget
+    [Environment]::SetEnvironmentVariable('JDK_17', $env:JAVA_HOME, 'Machine')
 
     winget install --id=Microsoft.OpenJDK.16 --source=winget
     
@@ -33,16 +32,24 @@ function Install-CoreJavaTools() {
     winget install --id=JetBrains.IntelliJIDEA.Community --source=winget
 }
 
-function Install-CoreWebTools() {
+function Install-WebTools() {
     winget install --id=OpenJS.NodeJS --source=winget
 
     winget install --id=Postman.Postman --source=winget
+}
+
+function Install-MiscellaneousTools() {
+    winget install --id=Docker.DockerDesktop --source=winget
+
+    winget install --id=Python.Python.3.11 --source=winget
 }
 
 function Install-CommunicationTools() {
     winget install --id=SlackTechnologies.Slack --source=winget
 
     winget install --id=Discord.Discord --source=winget
+
+    winget install --id=Zoom.Zoom --source=winget
 }
 
 # TODO: Install preview tools based on a switch from initialize script
@@ -56,9 +63,10 @@ function Install-PreviewTools() {
 Write-Host "Installing local development tools..."
 
 Install-PowerUserTools
-Install-CoreDotNetTools
-Install-CoreJavaTools
-Install-CoreWebTools
+Install-DotNetTools
+Install-JavaTools
+Install-WebTools
+Install-MiscellaneousTools
 Install-CommunicationTools
 Install-PreviewTools
 
